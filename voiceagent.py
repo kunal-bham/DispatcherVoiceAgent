51ca9405b97faadc41e0e03e17cef0e92693ea65
Kunal Bham <kunal14202@gmail.com>
2025-03-29 13:15:33 -0500

    starting from scratch using OpenAI TTS STT

--- voiceagent.py
+++ voiceagent.py
@@ -1,31 +1,4 @@
-# `pip3 install assemblyai` (macOS)
-# `pip install assemblyai` (Windows)
-
-import os
-import assemblyai as aai
-from elevenlabs.client import ElevenLabs
-from elevenlabs import stream
-import ollama
-
 # Step 1: Speech to Text User to AI
-# Step 2: Real-Time Transcription with AssemblyAI  
-# Step 3: AI Response with GPT
-# Step 4: Live Audio Streaming. Text-to-Speech with ElevenLabs
-
-class AIVoiceAgent:
-    def __init__(self):
-        aai.settings.api_key = "ASSEMBLYAI_API_KEY"
-        self.client = ElevenLabs(
-            api_key = "ELEVENLABS_API_KEY"
-        )
-
-        self.transcriber = None
-
-        self.full_transcript = [
-            {"role":"system", "content":"You are a language model, answer the questions being asked in less than 300 characters."},
-        ]
-
-
-
-
-
+# Step 2: Real-Time Transcription with OpenAI Whisper
+# Step 3: AI Response with gpt wrapper
+# Step 4: Live Audio Streaming. Text-to-Speech with OpenAI
