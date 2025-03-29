4c6ca58c4086c5bc41cb01cfa43d1456f60a8c78
Kunal Bham <kunal14202@gmail.com>
2025-03-29 13:44:49 -0500

    Speech to Text working

--- voiceagent.py
+++
@@ -1,4 +0,0 @@
-# Step 1: Speech to Text User to AI
-# Step 2: Real-Time Transcription with OpenAI Whisper
-# Step 3: AI Response with gpt wrapper
-# Step 4: Live Audio Streaming. Text-to-Speech with OpenAI
